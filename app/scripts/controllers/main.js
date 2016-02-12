751d91213c2713260d0c76f6b36579c4b574c8c9
Chris <christoph.kralj@gmail.com>
2016-02-12 16:49:54 +0100

    Implemented application header.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,5 +12,29 @@ angular.module('tagrefineryGuiApp')
   .controller('MainCtrl', ["$scope", "socket", "uiGridConstants", function ($scope, socket, uiGridConstants) {
    var that = this;
 
+   var connectionStatus = false;
+   $scope.$parent.disconnected = true;
+
+   socket.on('connected', function(data) {
+       	that.connectionStatus = true;
+       	$scope.$parent.disconnected = false;
+   });
+
+    socket.on('disconnect', function(data) {
+       	that.connectionStatus = false;
+       	$scope.$parent.disconnected = true;
+       	$scope.$parent.activeTabs[0] = true;
+   });
+
+    socket.on('connect_error', function(data) {
+       	that.connectionStatus = false;
+       	$scope.$parent.disconnected = true;
+       	$scope.$parent.activeTabs[0] = true;
+   });
+
+   that.reconnect = function() 
+   {
+       	socket.reconnect();
+   };
 
  }]);
--- app/scripts/services/socket.js
+++ app/scripts/services/socket.js
@@ -11,27 +11,36 @@
 angular.module('tagrefineryGuiApp')
   .factory('socket', ["$rootScope", function ($rootScope) {
 
-        var socketio = io.connect('http://localhost:9092',{
-            reconnection: false,
-        });
+      var socketio = io.connect('http://localhost:9092',{
+          reconnection: false,
+          'reconnectionAttempts': 5
+      });
+
+      var connect = function(e, callback) {
+        socketio.io.reconnect();
+      }
 
       var onEvent = function (e, callback) {
            socketio.on(e, function() {
-var args = arguments; $rootScope.$apply(function() {
-callback.apply(socketio, args); });
-}); };
+              var args = arguments; $rootScope.$apply(function() {
+                callback.apply(socketio, args); 
+              });
+            }); 
+         };
       
-      var emitEvent = function (e, data, callback) {
+      var emitEvent = function (e, data, callback) { 
            socketio.emit(e, data, function() {
-var args = arguments; $rootScope.$apply(function() {
-               if (callback) {
-                 callback.apply(socketio, args);
-} });
-}); };
+              var args = arguments; $rootScope.$apply(function() {
+                if (callback) {
+                callback.apply(socketio, args);
+              } });
+            }); 
+         };
 
       return {
           on: onEvent,
-          emit: emitEvent
+          emit: emitEvent,
+          reconnect: connect
       };
 
   }]);
