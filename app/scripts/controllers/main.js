3f1af66785e42b895c6a594f21d9cad142fbe9fa
Chris <christoph.kralj@gmail.com>
2016-02-16 18:54:14 +0100

    Import file is on gui side finished.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,12 +12,18 @@ angular.module('tagrefineryGuiApp')
   .controller('MainCtrl', ["$scope", "socket", "uiGridConstants", function ($scope, socket, uiGridConstants) {
    var that = this;
 
-   var connectionStatus = false;
+  that.connectionStatus = false;
+  that.dataLoaded = false;
+
    $scope.$parent.disconnected = true;
 
    socket.on('connect', function(data) {
        	that.connectionStatus = true;
-       	$scope.$parent.disconnected = false;
+
+        if($scope.data.length > 0)
+        {
+          $scope.$parent.disconnected = false;
+        }
    });
 
     socket.on('disconnect', function(data) {
@@ -32,9 +38,45 @@ angular.module('tagrefineryGuiApp')
        	$scope.$parent.activeTabs[0] = true;
    });
 
+  socket.on('importedData', function(data) {
+        that.connectionStatus = false;
+        $scope.$parent.disconnected = true;
+        $scope.$parent.activeTabs[0] = true;
+   });
+
    that.reconnect = function() 
    {
        	socket.reconnect();
    };
+
+   that.import = function() 
+   {
+      that.dataLoaded = true;
+
+      socket.emit("applyImportedData",$scope.data);
+   };
+
+      // Grid
+  $scope.data = [];
+  $scope.gridOptions = {
+    enableGridMenu: true,
+    showGridFooter: true,
+    enableColumnMenus: false,
+    enableFiltering: true,
+    rowEditWaitInterval: -1,
+    data: 'data',
+    importerDataAddCallback: function ( grid, newObjects ) {
+      $scope.data = $scope.data.concat( newObjects );
+      $scope.gridApi.core.notifyDataChange( uiGridConstants.dataChange.ALL );
+    },
+    onRegisterApi: function(gridApi){
+      $scope.gridApi = gridApi;
+    }, 
+    columnDefs: [
+    { field: 'tag', minWidth: 100, width: "*"},
+    { field: 'item', minWidth: 100, width: "*"},
+    { field: 'weight', minWidth: 100, width: "*"}
+    ]
+  };
 
  }]);
