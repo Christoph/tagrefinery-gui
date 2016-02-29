d0d8b7b5be9b4ee2d2716b0bc7ea78967c55969d
Chris <christoph.kralj@gmail.com>
2016-02-29 17:53:14 +0100

    Updated post params task.

--- app/scripts/controllers/postprocessingparams.js
+++ app/scripts/controllers/postprocessingparams.js
@@ -14,8 +14,11 @@ angular.module('tagrefineryGuiApp')
     var that = this;
 
     that.useAllWords = true;
+    that.newUseAllWords = true;
     that.minWordLength = 3;
+    that.newMinWordLength = 3;
     that.split = true;
+    that.newSplit = true;
 
     ////////////////////////////////////////////////
     // Socket functions
@@ -23,22 +26,32 @@ angular.module('tagrefineryGuiApp')
 
     socket.on('postAllParams', function (data) {
       that.useAllWords = data;
+      that.newUseAllWords = data;
     });
 
     socket.on('postSplitParams', function (data) {
       that.split = data;
+      that.newSplit = data;
     });
 
     socket.on('postLengthParams', function (data) {
       that.minWordLength = data;
+      that.newMinWordLength = data;
     });
 
     that.apply = function () {
       socket.emit("applyPostParams", JSON.stringify([{minWordLength: that.minWordLength, useAll: that.useAllWords, split: that.split}]));
 
-      socket.emit("applyPostLength", that.minWordLength);
-      socket.emit("applyPostAll", that.useAllWords);
-      socket.emit("applyPostSplit", that.split);
+      that.params.$setPristine();
     };
+
+    that.undo = function()
+    {
+      that.newUseAllWords = that.useAllWords;
+      that.newMinWordLength = that.minWordLength;
+      that.newSplit = that.split;
+
+      that.params.$setPristine();
+    }
 
   }]);
