c2d03d852bcef437fb660fc0fd30fe43c32f804f
Chris <christoph.kralj@gmail.com>
2016-03-05 12:45:07 +0100

    Added spinner directives.

---
+++ app/scripts/directives/spinrect.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name tagrefineryGuiApp.directive:Spinner
+ * @description
+ * # d3Hist
+ */
+angular.module('tagrefineryGuiApp')
+  .directive('spinRect', function () {
+    return {
+      replace: true,
+      template:'<div class="sk-cube-grid"> <div class="sk-cube sk-cube1"></div> <div class="sk-cube sk-cube2"></div> <div class="sk-cube sk-cube3"></div> <div class="sk-cube sk-cube4"></div> <div class="sk-cube sk-cube5"></div> <div class="sk-cube sk-cube6"></div> <div class="sk-cube sk-cube7"></div> <div class="sk-cube sk-cube8"></div> <div class="sk-cube sk-cube9"></div> </div>'
+    };
+  });
