3241ee83573920f1c30ab4807d29da4365277b20
Chris <christoph.kralj@gmail.com>
2016-03-20 01:33:36 +0100

    Its now possible to use multiple silders on the same page

--- app/scripts/directives/d3slider.js
+++ app/scripts/directives/d3slider.js
@@ -29,16 +29,16 @@ angular.module('tagrefineryGuiApp')
           var current = Math.max(0, Math.min(width, d3.event.x));
           scope.data = current;
 
-          marker.select(".outer")
+          marker.select(".outer."+scope.name)
             .attr("cx", current);
 
-          marker.select(".inner")
+          marker.select(".inner."+scope.name)
             .attr("cx", current);
 
-          d3.selectAll(".left")
+          d3.selectAll(".left."+scope.name)
             .attr("width", function(d) { return current; });
 
-          d3.selectAll(".right")
+          d3.selectAll(".right."+scope.name)
             .attr("width", function(d) { return width - current; })
             .attr("x", function(d) { return current; });
 
@@ -49,30 +49,28 @@ angular.module('tagrefineryGuiApp')
           {
             leftOp = 0;
             rightOp = 1;
-            marker.select(".outer").classed({'right': true, 'left': false})
           }
           else
           {
             leftOp = 1;
             rightOp = 0;
-            marker.select(".outer").classed({'right': false, 'left': true})
           }
 
-          d3.selectAll(".leftlabel")
+          d3.selectAll(".leftlabel."+scope.name)
             .attr("opacity", leftOp)
             .attr("x", function(d) { return current - heigth - 5})
             .text(formatCount(x.invert(current)));
 
-          d3.selectAll(".rightlabel")
+          d3.selectAll(".rightlabel."+scope.name)
             .attr("opacity", rightOp)
             .attr("x", function(d) { return current + heigth/2 + 5})
             .text(formatCount(x.invert(current)));
 
-          //scope.callBack({value: x.invert(scope.data)});
+          scope.callBack({value: x.invert(scope.data)});
         }
 
         function dragEnd() {
-          scope.callBack({value: x.invert(scope.data)});
+          //scope.callBack({value: x.invert(scope.data)});
         }
 
         svg = d3.select(element[0]).append("svg")
@@ -107,7 +105,7 @@ angular.module('tagrefineryGuiApp')
           .attr('transform', 'translate(20, 10)');
 
         bodyG.append("rect")
-          .attr("class", "left")
+          .attr("class", "left "+scope.name)
           .attr("y", 0)
           .attr("height", heigth)
           .on("click", function(d) {
@@ -117,11 +115,11 @@ angular.module('tagrefineryGuiApp')
           });
 
         bodyG.append("text")
-          .attr("class","leftlabel value")
+          .attr("class","leftlabel value "+scope.name)
           .attr("y", heigth/2+5);
 
         bodyG.append("rect")
-          .attr("class", "right")
+          .attr("class", "right "+scope.name)
           .attr("y", 0)
           .attr("height", heigth)
           .on("click", function(d) {
@@ -131,38 +129,38 @@ angular.module('tagrefineryGuiApp')
           });
 
         bodyG.append("text")
-          .attr("class","rightlabel value")
+          .attr("class","rightlabel value "+scope.name)
           .attr("y", heigth/2+5)
           .text(function(d) { return formatCount(x.invert(scope.data)); });
 
         marker = bodyG.append("g")
-          .attr("class", "marker")
+          .attr("class", "marker "+scope.name)
           .call(drag);
 
         marker.append("circle")
-          .attr("class", "outer")
+          .attr("class", "outer left "+scope.name)
           .attr("r", 20)
           .attr("cy", heigth/2);
 
         marker.append("circle")
-          .attr("class", "inner")
+          .attr("class", "inner "+scope.name)
           .attr("r", 10)
           .attr("cy", heigth/2)
           .attr("fill", "black");
-      }
+      };
 
       that.render = function(scope)
       {
-        marker.select(".outer")
+        marker.select(".outer."+scope.name)
           .attr("cx", scope.data);
 
-        marker.select(".inner")
+        marker.select(".inner."+scope.name)
           .attr("cx", scope.data);
 
-        d3.selectAll(".left")
+        d3.selectAll(".left."+scope.name)
           .attr("width", scope.data);
 
-        d3.selectAll(".right")
+        d3.selectAll(".right."+scope.name)
           .attr("width", function(d) { return width - scope.data; })
           .attr("x", function(d) { return scope.data; });
 
@@ -173,25 +171,23 @@ angular.module('tagrefineryGuiApp')
         {
           leftOp = 0;
           rightOp = 1;
-          marker.select(".outer").classed({'right': true, 'left': false})
         }
         else
         {
           leftOp = 1;
           rightOp = 0;
-          marker.select(".outer").classed({'right': false, 'left': true})
         }
 
-        d3.selectAll(".leftlabel")
+        d3.selectAll(".leftlabel."+scope.name)
           .attr("opacity", leftOp)
           .attr("x", function(d) { return scope.data - heigth - 5})
           .text(formatCount(x.invert(scope.data)));
 
-        d3.selectAll(".rightlabel")
+        d3.selectAll(".rightlabel."+scope.name)
           .attr("opacity", rightOp)
           .attr("x", function(d) { return scope.data + heigth/2 + 5})
           .text(formatCount(x.invert(scope.data)));
-      }
+      };
 
       that.scale = function(value)
       {
@@ -210,6 +206,7 @@ angular.module('tagrefineryGuiApp')
       link: function(scope, element, attrs, ctrl) {
         scope.heigth = parseInt(attrs.heigth) || 70;
         scope.xLabel = attrs.xlabel || "x-label";
+        scope.name = attrs.name || "slider";
         scope.data = 0;
 
         $timeout(function () {
